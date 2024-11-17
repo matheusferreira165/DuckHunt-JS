527ee6c9b8708fc45538d2398c18458ba0498647
Matheus Ferreira <matheus.ferreira165@outlook.com>
2024-11-17 20:47:02 -0300

    feat: normalize coordinates to move aim

--- duckhunt-game-js/src/modules/Aim.js
+++ duckhunt-game-js/src/modules/Aim.js
@@ -1,4 +1,5 @@
 import Character from './Character';
+import { Point } from 'pixi.js';
 
 class Aim extends Character{
   /**
@@ -16,14 +17,30 @@ class Aim extends Character{
     this.width = 40;
     this.height = 40;
     this.visible = true;
-    this.position.set(300, 350);
+    this.position.set(400, 300);
   }
 
   move(x, y) {
-    const localPosition = this.parent.toLocal({ x, y });
-    this.position.set(localPosition.x, localPosition.y);
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
 
+    const centerX = windowWidth / 2;
+    const centerY = windowHeight / 2;
+    const amplitude = 2000;
+
+    const alphaRad = x  * Math.PI / 180;
+    const betaRad = y  * Math.PI / 90;
+
+    const normalized_x = (centerX - amplitude * Math.sin(alphaRad));
+    const normalized_y = (centerY - amplitude * Math.sin(betaRad));
+
+    const coord = new Point(normalized_x, normalized_y);
+
+    const localPosition = this.parent.toLocal({x: coord.x, y: coord.y});
+
+    this.position.set(localPosition.x, localPosition.y);
   }
+
 }
 
 export default Aim;
