6dd678aee594d2aedbb0508b473198154226e412
Matheus Ferreira <matheus.ferreira165@outlook.com>
2025-02-04 22:39:07 -0300

    chroe: change to http

--- duckhunt-game-controller/index.js
+++ duckhunt-game-controller/index.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const https = require('https');
+const http = require('http');
 
 const app = express();
 const PORT = 3000;
-
-const privateKey = fs.readFileSync(path.join(__dirname, 'private.key'), 'utf8');
-const certificate = fs.readFileSync(path.join(__dirname, 'certificate.crt'), 'utf8');
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-https.createServer({ key: privateKey, cert: certificate }, app).listen(PORT, () => {
-    console.log(`Servidor HTTPS rodando em https://localhost:${PORT}`);
+http.createServer(app).listen(PORT, () => {
+    console.log(`Servidor HTTP rodando em http://localhost:${PORT}`);
 });
